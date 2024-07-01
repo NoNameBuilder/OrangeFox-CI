bca78519e67986a846019c8f6fd20c50bf43472a
NoNameBuilder <163744362+NoNameBuilder@users.noreply.github.com>
2024-07-01 18:44:25 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/NoNameBuilder/WorkingDTFORA24.git -b fox"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="a24"
+export OEM="samsung"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -22,8 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
-
+export EXTRA_CMD="export OF_MAINTAINER=ayoooo"
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
 export OF_USE_LATEST_MAGISK=true
